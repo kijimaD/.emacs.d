19e112abb29aa4ef05f2e090c0117cc18bd5fa29
kijima <norimaking777@gmail.com>
2019-12-12 17:25:47 +0900

    django-modeを追加した。 django-modeのキーバインドを修正した。

--- elpa/web-mode-20191104.1859/web-mode-autoloads.el
+++ elpa/web-mode-20191104.1859/web-mode-autoloads.el
@@ -1,10 +1,10 @@
 ;;; web-mode-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "web-mode" "web-mode.el" (22677 31535 630116
-;;;;;;  741000))
+;;;### (autoloads nil "web-mode" "web-mode.el" (24044 55977 366557
+;;;;;;  989000))
 ;;; Generated autoloads from web-mode.el
 
 (autoload 'web-mode "web-mode" "\
