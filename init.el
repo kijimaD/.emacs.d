cbcf61ce5494ba100f9a5e3f14693f39024b1ee2
Kijima Daigo <norimaking777@gmail.com>
2020-10-11 01:16:24 +0900

    Add settings for mac

--- init.el
+++ init.el
@@ -7,30 +7,26 @@
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
    ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
- '(custom-enabled-themes (quote (tango-dark)))
+ '(custom-enabled-themes '(tango-dark))
  '(custom-safe-themes
-   (quote
-    ("67e998c3c23fe24ed0fb92b9de75011b92f35d3e89344157ae0d544d50a63a72" "5cd0afd0ca01648e1fff95a7a7f8abec925bd654915153fb39ee8e72a8b56a1f" "06ffa2bf4e891580bfe6a5ce68d0909ed9c4278e5234ede6b3ba459ef35d9a1b" "dd4db38519d2ad7eb9e2f30bc03fba61a7af49a185edfd44e020aa5345e3dca7" "e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "62c80c6889010c3f0656e81ae201754058fd44743076f8dc56c595c2b9b5e298" "e1994cf306356e4358af96735930e73eadbaf95349db14db6d9539923b225565" "357d5abe6f693f2875bb3113f5c031b7031f21717e8078f90d9d9bc3a14bcbd8" "e8825f26af32403c5ad8bc983f8610a4a4786eb55e3a363fa9acb48e0677fe7e" "cdd26fa6a8c6706c9009db659d2dffd7f4b0350f9cc94e5df657fa295fffec71" "18a33cdb764e4baf99b23dcd5abdbf1249670d412c6d3a8092ae1a7b211613d5" "fe243221e262fe5144e89bb5025e7848cd9fb857ff5b2d8447d115e58fede8f7" "66881e95c0eda61d34aa7f08ebacf03319d37fe202d68ecf6a1dbfd49d664bc3" "b9293d120377ede424a1af1e564ba69aafa85e0e9fd19cf89b4e15f8ee42a8bb" "28ec8ccf6190f6a73812df9bc91df54ce1d6132f18b4c8fcc85d45298569eb53" "3eb93cd9a0da0f3e86b5d932ac0e3b5f0f50de7a0b805d4eb1f67782e9eb67a4" "251348dcb797a6ea63bbfe3be4951728e085ac08eee83def071e4d2e3211acc3" "6db9acac88c82f69296751e6c6d808736d6ff251dcb34a1381be86efc14fef54" "64ca5a1381fa96cb86fd6c6b4d75b66dc9c4e0fc1288ee7d914ab8d2638e23a9" "721bb3cb432bb6be7c58be27d583814e9c56806c06b4077797074b009f322509" "946e871c780b159c4bb9f580537e5d2f7dba1411143194447604ecbaf01bd90c" "ace9f12e0c00f983068910d9025eefeb5ea7a711e774ee8bb2af5f7376018ad2" "55d31108a7dc4a268a1432cd60a7558824223684afecefa6fae327212c40f8d3" default)))
+   '("67e998c3c23fe24ed0fb92b9de75011b92f35d3e89344157ae0d544d50a63a72" "5cd0afd0ca01648e1fff95a7a7f8abec925bd654915153fb39ee8e72a8b56a1f" "06ffa2bf4e891580bfe6a5ce68d0909ed9c4278e5234ede6b3ba459ef35d9a1b" "dd4db38519d2ad7eb9e2f30bc03fba61a7af49a185edfd44e020aa5345e3dca7" "e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "62c80c6889010c3f0656e81ae201754058fd44743076f8dc56c595c2b9b5e298" "e1994cf306356e4358af96735930e73eadbaf95349db14db6d9539923b225565" "357d5abe6f693f2875bb3113f5c031b7031f21717e8078f90d9d9bc3a14bcbd8" "e8825f26af32403c5ad8bc983f8610a4a4786eb55e3a363fa9acb48e0677fe7e" "cdd26fa6a8c6706c9009db659d2dffd7f4b0350f9cc94e5df657fa295fffec71" "18a33cdb764e4baf99b23dcd5abdbf1249670d412c6d3a8092ae1a7b211613d5" "fe243221e262fe5144e89bb5025e7848cd9fb857ff5b2d8447d115e58fede8f7" "66881e95c0eda61d34aa7f08ebacf03319d37fe202d68ecf6a1dbfd49d664bc3" "b9293d120377ede424a1af1e564ba69aafa85e0e9fd19cf89b4e15f8ee42a8bb" "28ec8ccf6190f6a73812df9bc91df54ce1d6132f18b4c8fcc85d45298569eb53" "3eb93cd9a0da0f3e86b5d932ac0e3b5f0f50de7a0b805d4eb1f67782e9eb67a4" "251348dcb797a6ea63bbfe3be4951728e085ac08eee83def071e4d2e3211acc3" "6db9acac88c82f69296751e6c6d808736d6ff251dcb34a1381be86efc14fef54" "64ca5a1381fa96cb86fd6c6b4d75b66dc9c4e0fc1288ee7d914ab8d2638e23a9" "721bb3cb432bb6be7c58be27d583814e9c56806c06b4077797074b009f322509" "946e871c780b159c4bb9f580537e5d2f7dba1411143194447604ecbaf01bd90c" "ace9f12e0c00f983068910d9025eefeb5ea7a711e774ee8bb2af5f7376018ad2" "55d31108a7dc4a268a1432cd60a7558824223684afecefa6fae327212c40f8d3" default))
  '(fci-rule-color "#2e2e2e")
  '(fringe-mode 0 nil (fringe))
- '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
+ '(highlight-changes-colors '("#d33682" "#6c71c4"))
  '(highlight-tail-colors
-   (quote
-    (("#eee8d5" . 0)
+   '(("#eee8d5" . 0)
      ("#B4C342" . 20)
      ("#69CABF" . 30)
      ("#69B7F0" . 50)
      ("#DEB542" . 60)
      ("#F2804F" . 70)
      ("#F771AC" . 85)
-     ("#eee8d5" . 100))))
+     ("#eee8d5" . 100)))
  '(line-number-mode nil)
  '(nrepl-message-colors
-   (quote
-    ("#336c6c" "#205070" "#0f2050" "#806080" "#401440" "#6c1f1c" "#6b400c" "#23733c")))
+   '("#336c6c" "#205070" "#0f2050" "#806080" "#401440" "#6c1f1c" "#6b400c" "#23733c"))
  '(org-agenda-files
-   (quote
-    ("/home/daigo3/org/art.org" "/home/daigo3/org/book.org" "/home/daigo3/org/bousai.org" "/home/daigo3/org/chinese.org" "/home/daigo3/org/chinese_learn.org" "/home/daigo3/org/config.org" "/home/daigo3/org/data.org" "/home/daigo3/org/diary.org" "/home/daigo3/org/eng_portfolio.org" "/home/daigo3/org/entertainment.org" "/home/daigo3/org/gasyuku.org" "/home/daigo3/org/gomi.org" "/home/daigo3/org/gorakumath.org" "/home/daigo3/org/ibunkarikai.org" "/home/daigo3/org/index.org" "/home/daigo3/org/info.org" "/home/daigo3/org/jikanwari.org" "/home/daigo3/org/jikanwari2.org" "/home/daigo3/org/jinsei_to_deai.org" "/home/daigo3/org/jisinkazan.org" "/home/daigo3/org/kadai.jpn.org" "/home/daigo3/org/keikaku.org" "/home/daigo3/org/kyarikoi.org" "/home/daigo3/org/labor.org" "/home/daigo3/org/memo.org" "/home/daigo3/org/mokuhyo.org" "/home/daigo3/org/other.org" "/home/daigo3/org/other2.org" "/home/daigo3/org/reg.org" "/home/daigo3/org/roudouhou.org" "/home/daigo3/org/ryuugaku.org" "/home/daigo3/org/schedule.org" "/home/daigo3/org/speech_bukai.org" "/home/daigo3/org/taiiku.org" "/home/daigo3/org/think.org" "/home/daigo3/org/trip.org" "/home/daigo3/org/web.org" "/home/daigo3/org/writer.org")))
+   '("/home/daigo3/org/art.org" "/home/daigo3/org/book.org" "/home/daigo3/org/bousai.org" "/home/daigo3/org/chinese.org" "/home/daigo3/org/chinese_learn.org" "/home/daigo3/org/config.org" "/home/daigo3/org/data.org" "/home/daigo3/org/diary.org" "/home/daigo3/org/eng_portfolio.org" "/home/daigo3/org/entertainment.org" "/home/daigo3/org/gasyuku.org" "/home/daigo3/org/gomi.org" "/home/daigo3/org/gorakumath.org" "/home/daigo3/org/ibunkarikai.org" "/home/daigo3/org/index.org" "/home/daigo3/org/info.org" "/home/daigo3/org/jikanwari.org" "/home/daigo3/org/jikanwari2.org" "/home/daigo3/org/jinsei_to_deai.org" "/home/daigo3/org/jisinkazan.org" "/home/daigo3/org/kadai.jpn.org" "/home/daigo3/org/keikaku.org" "/home/daigo3/org/kyarikoi.org" "/home/daigo3/org/labor.org" "/home/daigo3/org/memo.org" "/home/daigo3/org/mokuhyo.org" "/home/daigo3/org/other.org" "/home/daigo3/org/other2.org" "/home/daigo3/org/reg.org" "/home/daigo3/org/roudouhou.org" "/home/daigo3/org/ryuugaku.org" "/home/daigo3/org/schedule.org" "/home/daigo3/org/speech_bukai.org" "/home/daigo3/org/taiiku.org" "/home/daigo3/org/think.org" "/home/daigo3/org/trip.org" "/home/daigo3/org/web.org" "/home/daigo3/org/writer.org"))
  '(org-log-into-drawer nil)
  '(org-pomodoro-expiry-time 120000)
  '(org-pomodoro-finished-sound
@@ -42,7 +38,7 @@
    "~/.emacs.d/elpa/org-pomodoro-20161119.226/resources/bell.wav")
  '(org-pomodoro-ticking-sound
    "~/.emacs.d/elpa/org-pomodoro-20161119.226/resources/tick.wav")
- '(org-pomodoro-ticking-sound-states (quote (:pomodoro :short-break :long-break)))
+ '(org-pomodoro-ticking-sound-states '(:pomodoro :short-break :long-break))
  '(org-startup-indented t)
  '(org2blog/wp-buffer-template
    "#+DATE: %s
@@ -56,17 +52,15 @@
  '(org2blog/wp-default-categories nil)
  '(org2blog/wp-show-post-in-browser nil)
  '(package-selected-packages
-   (quote
-    (tabbar company robe ctags-update rubocop auto-highlight-symbol ruby-electric smooth-scrolling auto-complete-exuberant-ctags helm-gtags git-gutter-fringe+ dokuwiki org-journal-list org-journal dumb-jump dokuwiki-mode django-mode company-jedi markdown-mode jedi org-plus-contrib elscreen hiwin org org-brain zenburn-theme web-mode wc-goal-mode w3m typing twittering-mode summarye speed-type sound-wav solarized-theme smooth-scroll rainbow-delimiters psession projectile-rails powerline-evil pomodoro perl-completion paredit package-utils org-pomodoro open-junk-file noctilux-theme mozc-popup mozc-im maxframe magit lispxmp jdee helm-migemo helm grandshell-theme google-translate github-theme forest-blue-theme flatland-theme fish-mode firecode-theme fcitx farmhouse-theme eww-lnum espresso-theme elisp-slime-nav eldoc-extension eclipse-theme debug-print ddskk col-highlight chess autumn-light-theme auto-save-buffers-enhanced auto-install auto-complete anzu anything-project anti-zenburn-theme ample-zen-theme ample-theme afternoon-theme ace-jump-mode 2048-game)))
- '(pdf-view-midnight-colors (quote ("#232333" . "#c7c7c7")))
+   '(slim-mode tabbar company robe ctags-update rubocop auto-highlight-symbol ruby-electric smooth-scrolling auto-complete-exuberant-ctags helm-gtags git-gutter-fringe+ dokuwiki org-journal-list org-journal dumb-jump dokuwiki-mode django-mode company-jedi markdown-mode jedi org-plus-contrib elscreen hiwin org org-brain zenburn-theme web-mode wc-goal-mode w3m typing twittering-mode summarye speed-type sound-wav solarized-theme smooth-scroll rainbow-delimiters psession projectile-rails powerline-evil pomodoro perl-completion paredit package-utils org-pomodoro open-junk-file noctilux-theme mozc-popup mozc-im maxframe magit lispxmp jdee helm-migemo helm grandshell-theme google-translate github-theme forest-blue-theme flatland-theme fish-mode firecode-theme fcitx farmhouse-theme eww-lnum espresso-theme elisp-slime-nav eldoc-extension eclipse-theme debug-print ddskk col-highlight chess autumn-light-theme auto-save-buffers-enhanced auto-install auto-complete anzu anything-project anti-zenburn-theme ample-zen-theme ample-theme afternoon-theme ace-jump-mode 2048-game))
+ '(pdf-view-midnight-colors '("#232333" . "#c7c7c7"))
  '(scroll-bar-mode nil)
  '(show-paren-mode t)
  '(tool-bar-mode nil)
  '(tooltip-mode nil)
  '(vc-annotate-background "#3b3b3b")
  '(vc-annotate-color-map
-   (quote
-    ((20 . "#dd5542")
+   '((20 . "#dd5542")
      (40 . "#CC5542")
      (60 . "#fb8512")
      (80 . "#baba36")
@@ -83,14 +77,13 @@
      (300 . "#528fd1")
      (320 . "#5180b3")
      (340 . "#6380b3")
-     (360 . "#DC8CC3"))))
+     (360 . "#DC8CC3")))
  '(vc-annotate-very-old-color "#DC8CC3")
  '(wc-goal-modeline-format "WC[%C%c/%tc]")
  '(when
       (or
        (not
-	(boundp
-	 (quote ansi-term-color-vector)))
+	(boundp 'ansi-term-color-vector))
        (not
 	(facep
 	 (aref ansi-term-color-vector 0))))))
@@ -100,7 +93,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "源ノ角ゴシック Code JP" :slant normal :weight bold :height 64 :width normal :foundry "ADBO")))))
+ '(default ((t (:family "源ノ角ゴシック Code JP" :slant normal :weight bold :height 84 :width normal :foundry "ADBO")))))
 
 ;; (set-fontset-font t 'japanese-jisx0208 (font-spec :family "Takao Pゴシック"))
 ;; (set-fontset-font t
@@ -257,7 +250,7 @@
 (require 'org-install)
 ;; キーバインドの設定
 (define-key global-map "\C-cl" 'org-store-link)
-(define-key global-map "\C-ca" 'org-agenda)
+;; (define-key global-map "\C-ca" 'org-agenda)
 (define-key global-map "\C-cr" 'org-remember)
 
 ;; 拡張子がorgのファイルを開いた時，自動的にorg-modeにする
@@ -315,7 +308,7 @@
 ;;; inbox.orgのサンプルにあわせ、今日から30日分の予定を表示させる
 (setq org-agenda-span 30)
 ;;; org-agendaで扱うorgファイルたち
-(setq org-agenda-files '("~/org/inbox.org" "~/org/daily-projects.org" "~/junk/2018-11-06-225709log.org"))
+(setq org-agenda-files '("~/org/inbox.org" "~/org/daily-projects.org"))
 ;;; C-c a aでagendaのメニューを表示する
 ;;; agendaには、習慣・スケジュール・TODOを表示させる
 (global-set-key (kbd "C-c a") 'org-agenda)
@@ -633,7 +626,7 @@
 ;; 使い捨てのファイルを開く
 (require 'open-junk-file)
 ;; ファイル名入力時に ~/junk/-年-月-日-時-秒.が出てくる
-(setq open-junk-file-format "~/Dropbox/junk/%Y-%m-%d-%H%M%S.")
+(setq open-junk-file-format "~/Documents/junk/%Y-%m-%d-%H%M%S.")
 (global-set-key (kbd "C-x C-z") 'open-junk-file)
 
 ;; 自動コンパイルを無効にするファイル名の正規表現
@@ -806,9 +799,9 @@
 (psession-mode 1)
 
 ;; 非アクティブウィンドウの背景色を設定
-(require 'hiwin)
-(hiwin-activate)
-(set-face-background 'hiwin-face "gray20")
+;; (require 'hiwin)
+;; (hiwin-activate)
+;; (set-face-background 'hiwin-face "gray20")
 
 ;; tabサイズ
 (setq default-tab-width 4)
@@ -958,7 +951,7 @@
 (setq org-journal-date-format "%x")
 (setq org-journal-time-format "<%Y-%m-%d %R> ")
 (setq org-journal-file-format "%Y%m%d.org")
-(setq org-journal-dir "~/Dropbox/documents/diary/org-journal")
+(setq org-journal-dir "~/Documents/diary/org-journal")
 
 ;; SSH
 (require 'tramp)
@@ -1022,15 +1015,16 @@
 (global-set-key [insert] 'eijiro-at-point)
 (put 'upcase-region 'disabled nil)
 
-(require 'w3m)
-(define-key w3m-mode-map (kbd "<up>") nil)
-(define-key w3m-mode-map (kbd "<down>") nil)
-(define-key w3m-mode-map (kbd "<left>") nil)
-(define-key w3m-mode-map (kbd "<right>") nil)
-(define-key w3m-mode-map (kbd "C-t") nil)
+;; (require 'w3m)
+;; (define-key w3m-mode-map (kbd "<up>") nil)
+;; (define-key w3m-mode-map (kbd "<down>") nil)
+;; (define-key w3m-mode-map (kbd "<left>") nil)
+;; (define-key w3m-mode-map (kbd "<right>") nil)
+;; (define-key w3m-mode-map (kbd "C-t") nil)
+
 (define-key dired-mode-map (kbd "C-t") nil)
 
-(setq w3m-default-display-inline-images t)
+;; (setq w3m-default-display-inline-images t)
 
 (require 'auto-highlight-symbol)
 (global-auto-highlight-symbol-mode t)
@@ -1046,7 +1040,7 @@
 (setq ctags-update-command "/usr/bin/ctags")
 ;;; 使う言語で有効にしよう
 (add-hook 'c-mode-common-hook  'turn-on-ctags-auto-update-mode)
-(add-hook 'ruby-mode-hook  'turn-on-ctags-auto-update-mode)
+;; (add-hook 'ruby-mode-hook  'turn-on-ctags-auto-update-mode)
 (add-hook 'emacs-lisp-mode-hook  'turn-on-ctags-auto-update-mode)
 
 (server-start)
@@ -1131,7 +1125,7 @@
 
 ;; Tab
 (require 'tabbar)
-(tabbar-mode 1)
+(tabbar-mode 0)
 ;; タブ上でマウスホイール操作無効
 (tabbar-mwheel-mode -1)
 ;; グループ化しない
@@ -1141,3 +1135,10 @@
 ;; キーに割り当てる
 (global-set-key (kbd "M-<right>") 'tabbar-forward-tab)
 (global-set-key (kbd "M-<left>") 'tabbar-backward-tab)
+
+;; Mac用
+(setq ns-command-modifier (quote meta))
+(setq ns-alternate-modifier (quote super))
+
+(setq ruby-insert-encoding-magic-comment nil)
+(setq ctags-auto-update-mode nil)
