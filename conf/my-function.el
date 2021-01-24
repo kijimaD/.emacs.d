318c468f1a0c75ebb418a9aeaccd061570206211
kijima <norimaking777@gmail.com>
2021-01-24 21:22:27 +0900

    Add my-functions

--- conf/my-function.el
+++ conf/my-function.el
@@ -15,11 +15,14 @@
 (global-set-key (kbd "C-x C-x") 'my-exchange-point-and-mark)
 
 ;; カーソルのあるword削除
-(defun my-kill-word ()
-  (interactive)
-  (easy-mark-word 0)
-  (backward-delete-char 1))
+(fset 'my-kill-word
+   "\300\C-w")
 (global-set-key (kbd "M-d") 'my-kill-word)
+
+;; カーソルのあるsexp削除
+(fset 'my-kill-sexp
+   "\200\C-w")
+(global-set-key (kbd "C-M-d") 'my-kill-sexp)
 
 ;; カレントパス取得
 (defun current-path ()
@@ -35,3 +38,18 @@
           (t
            (error-message-string "Fail to get path name.")
            ))))
+
+;; 時間計測
+(defun my-clock()
+  (interactive)
+  (org-clock-out)
+  (org-clock-in))
+(define-key org-mode-map (kbd "<henkan>") 'my-clock)
+
+;; i-searchで逆側にカーソルをセットする
+(defun my-isearch-done-opposite (&optional nopush edit)
+  "End current search in the opposite side of the match."
+  (interactive)
+  (funcall #'isearch-done nopush edit)
+  (when isearch-other-end (goto-char isearch-other-end)))
+(define-key isearch-mode-map (kbd "C-<return>")'my-isearch-done-opposite)
