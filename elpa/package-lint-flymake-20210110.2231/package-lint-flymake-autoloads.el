122b9470054410e953445bb6949d8f973f8c4cb8
kijima <norimaking777@gmail.com>
2021-02-28 01:42:24 +0900

    Compile flycheck

---
+++ elpa/package-lint-flymake-20210110.2231/package-lint-flymake-autoloads.el
@@ -0,0 +1,28 @@
+;;; package-lint-flymake-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "package-lint-flymake" "package-lint-flymake.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from package-lint-flymake.el
+
+(autoload 'package-lint-flymake-setup "package-lint-flymake" "\
+Setup package-lint integration with Flymake.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "package-lint-flymake" '("package-lint-flymake")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; package-lint-flymake-autoloads.el ends here
