09711946f94dcbb2a7a097af4b1b17f72a6955a8
kijima <norimaking777@gmail.com>
2021-03-07 21:37:14 +0900

    Add test

--- init.el
+++ init.el
@@ -97,7 +97,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Menlo" :slant normal :weight normal :height 90 :width normal :foundry "PfEd")))))
+ '(default ((t (:family "Menlo" :slant normal :weight normal :height 110 :width normal :foundry "PfEd")))))
 (set-fontset-font t 'japanese-jisx0208 (font-spec :family "ヒラギノ 角ゴ ProN"))
 
 ;; ==================================================
