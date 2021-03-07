541f0d30dcc4a7ecf7a659d1be92802e1171d2f1
kijima <norimaking777@gmail.com>
2021-03-07 22:14:00 +0900

    Comment out rainbow settings

--- conf/package-init.el
+++ conf/package-init.el
@@ -43,13 +43,13 @@
 ;; コーディング外観  ================
 (require 'rainbow-delimiters)
 (rainbow-delimiters-mode t)
-(require 'cl-lib)
-(require 'color)
-(cl-loop
- for index from 1 to rainbow-delimiters-max-face-count
- do
- (let ((face (intern (format "rainbow-delimiters-depth-%d-face" index))))
-   (cl-callf color-saturate-name (face-foreground face) 30)))
+;; (require 'cl-lib)
+;; (require 'color)
+;; (cl-loop
+;;  for index from 1 to rainbow-delimiters-max-face-count
+;;  do
+;;  (let ((face (intern (format "rainbow-delimiters-depth-%d-face" index))))
+;;    (cl-callf color-saturate-name (face-foreground face) 30)))
 
 (add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
 
