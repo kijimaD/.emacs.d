2d09f2add2a89f14533fde94ab95043616f33aef
kijima <norimaking777@gmail.com>
2021-03-26 23:20:30 +0900

    Add cask

---
+++ custom.el
@@ -0,0 +1,6 @@
+;;; custom.el ---                                    -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2021  kijima
+
+;; Author: kijima <kijima@kijima-Prime-and-Diginnos>
+;; Keywords:
