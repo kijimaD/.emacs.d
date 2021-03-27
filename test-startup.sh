0a0d44a3e66111a13914c78fb5d92b4ddb4477c3
kijima <norimaking777@gmail.com>
2021-03-27 13:06:59 +0900

    Fix ci script

--- test-startup.sh
+++ test-startup.sh
@@ -1,6 +1,12 @@
 #!/bin/sh -e
 # From https://github.com/purcell/emacs.d
 
+git clone https://github.com/cask/cask ~/.cask
+PATH="$HOME/.cask/bin:$PATH"
+cask upgrade
+cd ~/.emacs.d
+cask install
+
 echo "Attempting startup..."
 ${EMACS:=emacs} -nw --batch \
                 --eval '(let ((debug-on-error t)
