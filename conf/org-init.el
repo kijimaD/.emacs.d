24b37e9fd45c2b6ae0229cfe8487cab09935a5fe
kijima <norimaking777@gmail.com>
2021-03-28 12:45:37 +0900

    Add org-journal settings for markdown

--- conf/org-init.el
+++ conf/org-init.el
@@ -45,15 +45,18 @@
                                (ruby . t)
                                (emacs-lisp . t)))
 
-;; 日誌 ================
+;; 日誌(mark-down化) ================
 (require 'org-journal)
 (setq org-journal-date-format "%Y-%m-%d")
 (setq org-journal-time-format "%R ")
-(setq org-journal-file-format "%Y%m%d.org")
 (setq org-journal-dir (concat "~/" public-directory "/junk/diary/org-journal"))
 (setq org-journal-find-file 'find-file)
 (setq org-journal-hide-entries-p nil)
 (setq org-startup-folded 'showeverything)
+(setq org-journal-file-format "%Y%m%d.md")
+(setq org-journal-date-prefix "# ")
+(setq org-journal-time-prefix "## ")
+(define-derived-mode org-journal-mode markdown-mode (run-mode-hook))
 
 ;; 使い捨てのファイルを開く ================
 (require 'open-junk-file)
