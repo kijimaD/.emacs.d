e5ff3b42ba35102f63d22cb30b8e0415a37b5327
kijima <norimaking777@gmail.com>
2021-03-31 00:47:42 +0900

    Add doom theme

--- conf/theme-init.el
+++ conf/theme-init.el
@@ -1,6 +1,6 @@
-(setq custom-enabled-themes '(tango-dark))
+(setq custom-enabled-themes '(doom-wilmersdorf))
 (setq custom-safe-themes t)
-(setq-default custom-enabled-themes '(tango-dark))
+(setq-default custom-enabled-themes '(doom-wilmersdorf))
 
 ;; https://github.com/purcell/emacs.d
 ;; Ensure that themes will be applied even if they have not been customized
@@ -9,6 +9,15 @@
   (dolist (theme custom-enabled-themes)
     (unless (custom-theme-p theme)
       (load-theme theme)))
-  (custom-set-variables `(custom-enabled-themes (quote ,custom-enabled-themes))))
+  (custom-set-variables `(custom-enabled-themes (quote ,custom-enabled-themes)))
+
+  (doom-modeline-mode 1)
+  (setq doom-modeline-height 16)
+
+  (setq doom-modeline-buffer-file-name-style 'truncate-with-project)
+  (setq doom-modeline-icon t)
+  (setq doom-modeline-major-mode-icon nil)
+  (setq doom-modeline-minor-modes nil)
+  )
 
 (add-hook 'after-init-hook 'reapply-themes)
