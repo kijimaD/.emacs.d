8ea423ccc28d6729b8503023dc9cdcb0b48eeef1
kijima <norimaking777@gmail.com>
2021-05-04 13:00:53 +0900

    Add perspective settings

--- conf/workspace-init.el
+++ conf/workspace-init.el
@@ -1,23 +1,18 @@
 ;; workspace
-;; ワークスペース ================
 (require 'perspective)
+(setq persp-initial-frame-name "1")
 (persp-mode 1)
+
+(delete-other-windows)
+(split-window-right)
 
 ;; ワークスペース生成
 (mapc (lambda (i)
-        (persp-switch (int-to-string i)))
-      (number-sequence 0 4))
+        (persp-switch (int-to-string i))
+        (delete-other-windows)
+        (split-window-right))
+      (number-sequence 1 9))
 
-;;;; キーに登録する関数を返す関数
-;;(defun local-switch-workspace (i)
-;;  (lexical-let ((index i))
-;;    (lambda ()
-;;      (interactive)
-;;      (persp-switch (int-to-string index)))))
-
-;;
-;; 更新 2020/04/28
-;;
 (defun local-switch-workspace (index)
   `(lambda ()
      (interactive)
@@ -25,6 +20,6 @@
 
 ;; キーバインドの登録を行う
 (mapc (lambda (i)
-        (global-set-key (kbd (format "M-%d" i)) (local-switch-workspace i)));;
-      (number-sequence 0 4))
-(persp-switch "0")
+        (global-set-key (kbd (format "M-%d" i)) (local-switch-workspace i)))
+      (number-sequence 1 9))
+(persp-switch "1")
