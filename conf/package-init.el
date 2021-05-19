d0c2b544c194555dbcded6a70e80a0aa478161e1
kijima <norimaking777@gmail.com>
2021-05-19 09:46:46 +0900

    Add vterm-toggle settings

--- conf/package-init.el
+++ conf/package-init.el
@@ -470,6 +470,17 @@
 ;; 正規表現 ================
 (global-set-key (kbd "C-M-%") 'vr/query-replace)
 
-;; vterm ================
+;; vterm-toggle ================
 (global-set-key [f9] 'vterm-toggle)
 (global-set-key [C-f9] 'vterm-toggle-cd)
+
+(setq vterm-toggle-fullscreen-p nil)
+(add-to-list 'display-buffer-alist
+             '((lambda(bufname _) (with-current-buffer bufname (equal major-mode 'vterm-mode)))
+                (display-buffer-reuse-window display-buffer-at-bottom)
+                ;;(display-buffer-reuse-window display-buffer-in-direction)
+                ;;display-buffer-in-direction/direction/dedicated is added in emacs27
+                ;;(direction . bottom)
+                ;;(dedicated . t) ;dedicated is supported in emacs27
+                (reusable-frames . visible)
+                (window-height . 0.3)))
