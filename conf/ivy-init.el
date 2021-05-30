cd0ed8e0cef2b7b7af3c832ab10e15fcafc40fc8
kijima <norimaking777@gmail.com>
2021-05-30 17:29:22 +0900

    Adjust settings

--- conf/ivy-init.el
+++ conf/ivy-init.el
@@ -1,63 +1,61 @@
 ;; https://qiita.com/takaxp/items/2fde2c119e419713342b
+
 ;; ivy ================
-(when (require 'ivy nil t)
-  ;; M-o を ivy-hydra-read-action に割り当てる．
-  (when (require 'ivy-hydra nil t)
-    (setq ivy-read-action-function #'ivy-hydra-read-action))
+;; M-o を ivy-hydra-read-action に割り当てる．
+(when (require 'ivy-hydra nil t)
+  (setq ivy-read-action-function #'ivy-hydra-read-action))
 
-  ;; `ivy-switch-buffer' (C-x b) のリストに recent files と bookmark を含める．
-  (setq ivy-use-virtual-buffers t)
+;; Magit
+(setq magit-completing-read-function 'ivy-completing-read)
 
-  ;; ミニバッファでコマンド発行を認める
-  (when (setq enable-recursive-minibuffers t)
-    (minibuffer-depth-indicate-mode 1)) ;; 何回層入ったかプロンプトに表示．
+;; `ivy-switch-buffer' (C-x b) のリストに recent files と bookmark を含める．
+(setq ivy-use-virtual-buffers t)
 
-  ;; ESC連打でミニバッファを閉じる
-  (define-key ivy-minibuffer-map (kbd "<escape>") 'minibuffer-keyboard-quit)
+;; ミニバッファでコマンド発行を認める
+(when (setq enable-recursive-minibuffers t)
+  (minibuffer-depth-indicate-mode 1)) ;; 何回層入ったかプロンプトに表示．
 
-  ;; プロンプトの表示が長い時に折り返す（選択候補も折り返される）
-  (setq ivy-truncate-lines nil)
+;; ESC連打でミニバッファを閉じる
+(define-key ivy-minibuffer-map (kbd "<escape>") 'minibuffer-keyboard-quit)
 
-  ;; リスト先頭で `C-p' するとき，リストの最後に移動する
-  (setq ivy-wrap t)
+;; プロンプトの表示が長い時に折り返す（選択候補も折り返される）
+(setq ivy-truncate-lines nil)
 
-  ;; アクティベート
-  (ivy-mode 1))
+;; リスト先頭で `C-p' するとき，リストの最後に移動する
+(setq ivy-wrap t)
 
 (setq ivy-count-format "⚝%d╳%d⚝ ")
 
-(with-eval-after-load "magit"
-  (setq magit-completing-read-function 'ivy-completing-read))
+;; アクティベート
+(ivy-mode 1)
 
-(setq ivy-initial-inputs-alist
-      '((org-agenda-refile . "^")
-        (org-capture-refile . "^")
-        ;; (counsel-M-x . "^") ;; 削除．必要に応じて他のコマンドも除外する．
-        (counsel-describe-function . "^")
-        (counsel-describe-variable . "^")
-        (Man-completion-table . "^")
-        (woman . "^")))
+;; (setq ivy-initial-inputs-alist
+;;       '((org-agenda-refile . "^")
+;;         (org-capture-refile . "^")
+;;         ;; (counsel-M-x . "^") ;; 削除．必要に応じて他のコマンドも除外する．
+;;         (counsel-describe-function . "^")
+;;         (counsel-describe-variable . "^")
+;;         (Man-completion-table . "^")
+;;         (woman . "^")))
 
 ;; counsel ================
-(when (require 'counsel nil t)
-  (global-set-key (kbd "C-x C-b") 'counsel-ibuffer)
-  (global-set-key (kbd "C-x C-r") 'counsel-recentf)
-  (global-set-key (kbd "C-x C-f") 'counsel-find-file)
-  (global-set-key (kbd "C-x C-l") 'counsel-mark-ring)
-  (global-set-key (kbd "C-x C-u") 'ivy-resume)
-  (global-set-key (kbd "C-x C-g") 'counsel-git-grep)
-  (global-set-key (kbd "M-x") 'counsel-M-x)
-  (global-set-key (kbd "M-y") 'counsel-yank-pop)
-  (global-set-key (kbd "M-i") 'swiper-thing-at-point)
-  (global-set-key (kbd "C-c f") 'counsel-ag)
-  (global-set-key (kbd "C-c i") 'counsel-imenu)
-  (global-set-key (kbd "C-c y") 'ivy-yasnippet)
-  (global-set-key (kbd "C-c h") 'counsel-find-library)
-  (global-set-key [delete] 'counsel-apropos)
-  ;; bm
+(global-set-key (kbd "C-x C-b") 'counsel-ibuffer)
+(global-set-key (kbd "C-x C-r") 'counsel-recentf)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "C-x C-l") 'counsel-mark-ring)
+(global-set-key (kbd "C-x C-u") 'ivy-resume)
+(global-set-key (kbd "C-x C-g") 'counsel-git-grep)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "M-y") 'counsel-yank-pop)
+(global-set-key (kbd "M-i") 'swiper-thing-at-point)
+(global-set-key (kbd "C-c f") 'counsel-ag)
+(global-set-key (kbd "C-c i") 'counsel-imenu)
+(global-set-key (kbd "C-c y") 'ivy-yasnippet)
+(global-set-key (kbd "C-c h") 'counsel-find-library)
+(global-set-key [delete] 'counsel-apropos)
+;; bm
 
-  ;; アクティベート
-  (counsel-mode 1))
+(counsel-mode 1)
 
 ;; eldoc ================
 (with-eval-after-load "eldoc"
@@ -98,7 +96,7 @@
    ((((class color) (background light)) :foreground "#439943" :underline t)
     (((class color) (background dark)) :foreground "#33bb33" :underline t))))
 
-;; ag
+;; agのデフォルト入力値をthing-at-pointにする
 (defun ad:counsel-ag (f &optional initial-input initial-directory extra-ag-args ag-prompt caller)
   (apply f (or initial-input (ivy-thing-at-point))
          (unless current-prefix-arg
