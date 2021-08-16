6b6a43d7ea0b648255e947b2633dd37ec9cbda2c
kijima <norimaking777@gmail.com>
2021-08-16 14:19:27 +0900

    Add eshell prompt setting

--- conf/shell-init.el
+++ conf/shell-init.el
@@ -19,9 +19,79 @@
 
 (add-hook 'eshell-first-time-mode-hook 'efs/configure-eshell)
 (eshell-git-prompt-use-theme 'robbyrussell)
+(setq eshell-toggle-height-fraction 2)
 
 (global-set-key (kbd "C-M-;") 'eshell-toggle)
 (setq eshell-toggle-use-projectile-root t)
+
+(defun dw/get-prompt-path ()
+  (let* ((current-path (eshell/pwd))
+         (git-output (shell-command-to-string "git rev-parse --show-toplevel"))
+         (has-path (not (string-match "^fatal" git-output))))
+    (if (not has-path)
+        (abbreviate-file-name current-path)
+      (string-remove-prefix (file-name-directory git-output) current-path))))
+
+(defun dw/eshell-prompt ()
+  (let ((current-branch (magit-get-current-branch)))
+    (concat
+     "\n"
+     (propertize (system-name) 'face `(:foreground "#62aeed"))
+     (propertize " ❖ " 'face `(:foreground "gray"))
+     (propertize (dw/get-prompt-path) 'face `(:foreground "#82cfd3"))
+     (when current-branch
+       (concat
+        (propertize " ❖ " 'face `(:foreground "gray"))
+        (propertize current-branch 'face `(:foreground "#c475f0"))))
+     (propertize " ❖ " 'face `(:foreground "gray"))
+     (propertize (format-time-string "%I:%M:%S %p") 'face `(:foreground "#5a5b7f"))
+     (if (= (user-uid) 0)
+         (propertize "\n#" 'face `(:foreground "red2"))
+       (propertize "\nλ" 'face `(:foreground "#aece4a")))
+     (propertize " " 'face `(:foreground "white")))))
+
+(defun dw/eshell-configure ()
+  (push 'eshell-tramp eshell-modules-list)
+  (delq 'eshell-handle-ansi-color eshell-output-filter-functions)
+
+  ;; Save command history when commands are entered
+  (add-hook 'eshell-pre-command-hook 'eshell-save-some-history)
+
+  (add-hook 'eshell-before-prompt-hook
+            (lambda ()
+              (setq xterm-color-preserve-properties t)))
+
+  ;; Truncate buffer for performance
+  (add-to-list 'eshell-output-filter-functions 'eshell-truncate-buffer)
+
+  ;; We want to use xterm-256color when running interactive commands
+  ;; in eshell but not during other times when we might be launching
+  ;; a shell command to gather its output.
+  (add-hook 'eshell-pre-command-hook
+            (lambda () (setenv "TERM" "xterm-256color")))
+  (add-hook 'eshell-post-command-hook
+            (lambda () (setenv "TERM" "dumb")))
+
+  ;; Use completion-at-point to provide completions in eshell
+  (define-key eshell-mode-map (kbd "<tab>") 'completion-at-point)
+
+  ;; Initialize the shell history
+  (eshell-hist-initialize)
+
+
+  (setenv "PAGER" "cat")
+
+  (setq eshell-prompt-function      'dw/eshell-prompt
+        eshell-prompt-regexp        "^λ "
+        eshell-history-size         10000
+        eshell-buffer-maximum-lines 10000
+        eshell-hist-ignoredups t
+        eshell-highlight-prompt t
+        eshell-scroll-to-bottom-on-input t
+        eshell-prefer-lisp-functions nil))
+
+(add-hook 'eshell-first-time-mode-hook 'dw/eshell-configure)
+
 ;; vterm ================
 (setq term-prompt-regexp "^[^#$%>\n]*[#$%>] *")
 (setq vterm-max-scrollback 10000)
@@ -33,10 +103,10 @@
 (setq vterm-toggle-fullscreen-p nil)
 (add-to-list 'display-buffer-alist
              '((lambda(bufname _) (with-current-buffer bufname (equal major-mode 'vterm-mode)))
-                (display-buffer-reuse-window display-buffer-at-bottom)
-                ;;(display-buffer-reuse-window display-buffer-in-direction)
-                ;;display-buffer-in-direction/direction/dedicated is added in emacs27
-                ;;(direction . bottom)
-                ;;(dedicated . t) ;dedicated is supported in emacs27
-                (reusable-frames . visible)
-                (window-height . 0.3)))
+               (display-buffer-reuse-window display-buffer-at-bottom)
+               ;;(display-buffer-reuse-window display-buffer-in-direction)
+               ;;display-buffer-in-direction/direction/dedicated is added in emacs27
+               ;;(direction . bottom)
+               ;;(dedicated . t) ;dedicated is supported in emacs27
+               (reusable-frames . visible)
+               (window-height . 0.3)))
