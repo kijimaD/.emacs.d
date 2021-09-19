27ac6a941695a18d93a15a428d35339b7704d6c6
kijima <norimaking777@gmail.com>
2021-09-19 19:41:43 +0900

    Add elisp support packages

--- conf/package-init.el
+++ conf/package-init.el
@@ -573,3 +573,13 @@
 
 ;; ripgrep ================
 (rg-enable-default-bindings)
+
+;; Emacs Lisp ================
+(require 'paredit)
+(add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
+(add-hook 'lisp-interaction-mode-hook 'enable-paredit-mode)
+(add-hook 'lisp-mode-hook 'enable-paredit-mode)
+(add-hook 'ielm-mode-hook 'enable-paredit-mode)
+
+(require 'lispxmp)
+(define-key emacs-lisp-mode-map (kbd "C-<return>") 'lispxmp)
