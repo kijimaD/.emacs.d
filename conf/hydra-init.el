2890c28bbfea2ca6f3d42ee6348ea4b8382bbc6f
kijimaD <norimaking777@gmail.com>
2022-07-23 11:00:13 +0900

    feat: hydra settings

--- conf/hydra-init.el
+++ conf/hydra-init.el
@@ -1,7 +1,11 @@
+(use-package hydra-posframe
+  :straight (:host github :repo "Ladicle/hydra-posframe")
+  :hook (after-init . hydra-posframe-enable))
+
 (with-eval-after-load 'pretty-hydra
   ;; projectile-rails ================
   ;; https://mugijiru.github.io/.emacs.d/programming/rails/
-  (pretty-hydra-define pretty-hydra-projectile-rails-find (:separator "-" :color blue :foreign-keys warn :title "Projectile Rails" :quit-key "q")
+  (pretty-hydra-define pretty-hydra-projectile-rails-find (:separator "-" :color blue :foreign-keys warn :title "Projectile Rails")
     ("Current"
      (("M" projectile-rails-find-current-model      "Current model")
       ("V" projectile-rails-find-current-view       "Current view")
@@ -35,4 +39,51 @@
      (("R" projectile-rails-goto-routes   "routes.rb")
       ("G" projectile-rails-goto-gemfile  "Gemfile")
       ("D" projectile-rails-goto-schema   "schema.rb"))))
-  (define-key projectile-rails-mode-map (kbd "C-c r") 'pretty-hydra-projectile-rails-find/body))
+
+  (pretty-hydra-define+ pretty-hydra-henkan (:foreign-keys warn :title "Convenient Tools" :quit-key "g")
+    ("Media"
+     (("<prior>" kd/mint-volume-up "up")
+      ("<next>" kd/mint-volume-down "down"))
+
+     "Move"
+     (("a" counsel-apropos "apropos")
+      ("f" counsel-ag "ag")
+      ("h" counsel-find-library "lib")
+      ("i" counsel-imenu "imenu")
+      ("r" counsel-mark-ring "ring")
+      ("b" counsel-bookmark "bookmark"))
+
+     "Execute"
+     (("e" counsel-linux-app "run")
+      ("c" recompile "recompile")
+      ("s" counsel-search "google")
+      )
+
+     "Edit"
+     (("q" query-replace "replace")
+      ("y" ivy-yasnippet "yas")
+     )))
+
+  (define-key projectile-rails-mode-map (kbd "C-c r") 'pretty-hydra-projectile-rails-find/body)
+  (define-key global-map (kbd "<henkan>") 'pretty-hydra-henkan/body))
+
+;; major-mode-hydra ================
+
+(global-set-key (kbd "M-SPC") #'major-mode-hydra)
+
+(with-eval-after-load 'major-mode-hydra
+  (major-mode-hydra-define emacs-lisp-mode nil
+    ("Eval"
+     (("b" eval-buffer "buffer")
+      ("e" eval-defun "defun")
+      ("r" eval-region "region"))
+     "REPL"
+     (("I" ielm "ielm"))
+     "Test"
+     (("t" ert "prompt")
+      ("T" (ert t) "all")
+      ("F" (ert :failed) "failed"))
+     "Doc"
+     (("f" describe-function "function")
+      ("v" describe-variable "variable")
+      ("i" info-lookup-symbol "info lookup")))))
