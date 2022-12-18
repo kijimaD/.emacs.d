0d34f6d7e1267cb7725b016dc04219da0d94ae20
kijimaD <norimaking777@gmail.com>
2022-12-18 12:52:39 +0900

    refactor: startup command

--- test-startup.sh
+++ test-startup.sh
@@ -1,8 +1,15 @@
 #!/bin/sh -e
-# From https://github.com/purcell/emacs.d
+# based on https://github.com/purcell/emacs.d
 
 echo "Attempting startup..."
+
 ${EMACS:=emacs} -nw --batch \
-                --eval '(let ((url-show-status nil)))' \
-                -l ./init.el
+                --eval '(progn (setq debug-on-error t)
+                               (setq url-show-status nil)
+                               (setq user-emacs-directory default-directory)
+                               (setq user-init-file (expand-file-name "init.el"))
+                               (setq load-path (delq default-directory load-path))
+                               (load-file user-init-file)
+                               (run-hooks (quote after-init-hook)))'
+
 echo "Startup successful"
