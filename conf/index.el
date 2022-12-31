4d3d1ffae40e1816076993b8bdcd91268ca92b39
kijimaD <norimaking777@gmail.com>
2022-12-31 21:41:23 +0900

    feat: go config

--- conf/index.el
+++ conf/index.el
@@ -1215,12 +1215,10 @@
 (global-set-key (kbd "C-m") 'newline)
 (add-hook 'ruby-mode-hook 'smart-newline-mode)
 
-;;; ruby_on_railsモード
 (require 'projectile-rails)
 (projectile-rails-global-mode)
 (add-hook 'projectile-mode-hook 'projectile-rails-on)
 
-;; rspec ================
 (require 'rspec-mode)
 
 ;; Rspecの実行結果をスクロールして出力する
@@ -1232,7 +1230,6 @@
 (setq rspec-docker-command "docker-compose -f docker-compose.yml -f docker-compose-app.yml -f docker-compose-app.override.yml exec")
 (setq rspec-docker-cwd "")
 
-;; RAILS_ENV=testを追加
 (defun rspec-runner ()
   "Return command line to run rspec."
   (let ((bundle-command (if (rspec-bundle-p) "RAILS_ENV=test bundle exec " ""))
@@ -1243,16 +1240,11 @@
                 (concat rspec-rake-command " spec")
               rspec-spec-command))))
 
-;; (setq rspec-use-spring-when-possible nil)
 (setq rspec-use-spring-when-possible t)
-(defun rspec-spring-p ()
-  (and rspec-use-spring-when-possible
-       (stringp (executable-find "spring"))))
-;; spring
-;; bin/rspec
+  (defun rspec-spring-p ()
+    (and rspec-use-spring-when-possible
+         (stringp (executable-find "spring"))))
 
-;; シンタックスチェック ================
-;; flycheck と rubocop を連携させる
 (require 'rubocop)
 (add-hook 'ruby-mode-hook 'rubocop-mode)
 (add-hook 'ruby-mode-hook
@@ -1271,58 +1263,46 @@
           (file-name) ":" line ":" column ": " (or "E" "F") ": " (message) line-end))
   :modes (ruby-mode motion-mode))
 
-;; rinari
+(setq flycheck-ruby-rubocop-executable "bundle exec rubocop")
+
 (when (require 'rinari nil 'noerror)
   (require 'rinari)
   (add-hook 'ruby-mode-hook 'rinari-minor-mode))
 
-;; rspec-mode 用の snippet を認識させる
 (when (require 'rinari nil 'noerror)
   (require 'rspec-mode)
   (eval-after-load 'rspec-mode
     '(rspec-install-snippets)))
 
-(setq flycheck-ruby-rubocop-executable "bundle exec rubocop")
-
-;; 補完 ================
 (require 'ruby-electric)
 (add-hook 'ruby-mode-hook '(lambda ()
                              (ruby-electric-mode t)))
 
-;; 実行環境 ================
 (require 'quickrun)
 (global-set-key (kbd "<f8>") 'quickrun)
 
-;; pry
 (require 'inf-ruby)
 (setq inf-ruby-default-implementation "pry")
 (setq inf-ruby-eval-binding "Pry.toplevel_binding")
 ;; riなどのエスケープシーケンスを処理し、色付けする
 (add-hook 'inf-ruby-mode-hook 'ansi-color-for-comint-mode-on)
 
-;; slim-mode ================
 (unless (package-installed-p 'slim-mode)
   (package-refresh-contents) (package-install 'slim-mode))
 (add-to-list 'auto-mode-alist '("\\.slim?\\'" . slim-mode))
 
-;; yaml-mode ================
 (add-to-list 'auto-mode-alist '("\\.ya?ml$" . yaml-mode))
 
-;; マジックコメントを挿入しない
 (setq ruby-insert-encoding-magic-comment nil)
 
-;; 対応ブロックを光らせる時間を短くする
 (defcustom ruby-block-delay 0
-  "*Time in seconds to delay before showing a matching paren."
-  :type  'number
-  :group 'ruby-block)
+    "*Time in seconds to delay before showing a matching paren."
+    :type  'number
+    :group 'ruby-block)
 
-;; xmp(実行結果アノテーション)
 (require 'rcodetools)
 (define-key ruby-mode-map (kbd "C-<return>") 'xmp)
 
-;; activate robe
-;; CIでは実行しない
 (when window-system
   (progn
     ;; (inf-ruby)
@@ -1332,22 +1312,19 @@
 (require 'go-mode)
 (require 'ob-go)
 
-;; Go デバッガー
 (use-package dap-mode
-  :after lsp-mode
-  :hook
-  (lsp-mode . dap-mode)
-  (lsp-mode . dap-ui-mode)
-  :config
-  (dap-mode 1)
-  (require 'dap-hydra)
-  (require 'dap-dlv-go))
+    :after lsp-mode
+    :hook
+    (lsp-mode . dap-mode)
+    (lsp-mode . dap-ui-mode)
+    :config
+    (dap-mode 1)
+    (require 'dap-hydra)
+    (require 'dap-dlv-go))
 
 (setq dap-print-io t)
 (setq lsp-gopls-server-path "~/go/bin/gopls")
 (setq dap-dlv-go-delve-path "~/go/bin/dlv")
-
-(setq dap-print-io t)
 
 (require 'go-eldoc)
 (add-hook 'go-mode-hook 'go-eldoc-setup)
